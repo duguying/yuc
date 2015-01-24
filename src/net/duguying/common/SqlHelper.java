339a3b992b4bc9096b333e26bf859e34c83aed35
Rex Lee(李俊) <duguying2008@gmail.com>
2015-01-24 18:32:33 +0000

    add jdbc

--- src/net/duguying/common/SqlHelper.java
+++ src/net/duguying/common/SqlHelper.java
@@ -4,4 +4,14 @@ package net.duguying.common;
  * Created by rex on 2015/1/23.
  */
 public class SqlHelper {
+    public SqlHelper() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch(ClassNotFoundException ex){
+            System.out.println("Class com.mysql.jdbc.Driver dose not exist!");
+        }
+
+    }
+
+
 }
