9956981e81642a76f107f3220e7516f89b657b1f
Rex Lee(李俊) <duguying2008@gmail.com>
2015-01-24 19:32:51 +0000

    try mysql connection

--- src/net/duguying/common/BaseServlet.java
+++ src/net/duguying/common/BaseServlet.java
@@ -3,13 +3,15 @@ package net.duguying.common;
 import javax.servlet.*;
 import java.io.IOException;
 
+import net.duguying.common.SqlHelper;
+
 /**
  * Created by rex on 2015/1/18.
  */
 public class BaseServlet implements Servlet {
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
-
+        SqlHelper sql = new SqlHelper();
     }
 
     @Override
--- src/net/duguying/common/SqlHelper.java
+++ src/net/duguying/common/SqlHelper.java
@@ -1,15 +1,38 @@
 package net.duguying.common;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by rex on 2015/1/23.
  */
 public class SqlHelper {
     public SqlHelper() {
-        try{
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-        }catch(ClassNotFoundException ex){
+        } catch (ClassNotFoundException ex) {
             System.out.println("Class com.mysql.jdbc.Driver dose not exist!");
+            return;
         }
+
+        String url = "jdbc:mysql://localhost:3306/test";
+        String username = "root";
+        String password = "";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+            if (conn!=null){
+                System.out.println("Connect Mysql Success!");
+            }else{
+                System.out.println("Connect Mysql Failed!");
+                return;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Connect Mysql Failed!");
+            return;
+        }
+
 
     }
 
